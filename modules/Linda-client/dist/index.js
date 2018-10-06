1e53bb1af52b1363babb7bbaba018f5e30802f7d
saji-ryu <rsajiki@gmail.com>
2018-10-07 00:00:11 +0900

    dist削除

--- modules/Linda-client/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var lindaClient_1 = require("./lindaClient");
-exports.default = lindaClient_1.default;
-// module.exports = LindaClient;
-//const lindaClient = new LindaClient();
-//# sourceMappingURL=index.js.map
--- modules/Linda-server/dist/db/memoryDB.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var memoryDB = {
-    test: [{ _id: 0, time: Date.now(), type: "init" }],
-};
-exports.default = memoryDB;
-//# sourceMappingURL=memoryDB.js.map
