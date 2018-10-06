1f556746bf47e788073d6d25da5e6aea413fac77
saji-ryu <rsajiki@gmail.com>
2018-10-07 00:16:46 +0900

    URL ミス

--- modules/Linda-server/src/pages/home.tsx
+++ modules/Linda-server/src/pages/home.tsx
@@ -2,17 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 type Props = {};
-type State = {
-  tuples: Array<any>;
-  watchingTuple: any;
-};
 
 const Home = props => (
   <div>
     <h1>Welcome to Linda</h1>
-    <a href="http://https://github.com/saji-ryu/Linda/modules/Linda-server">
-      github
-    </a>
+    <a href="https://github.com/saji-ryu/Linda/modules/Linda-server">github</a>
   </div>
 );
 ReactDOM.render(<Home />, document.getElementById("content"));
